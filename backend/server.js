4a8542f70345d75cd8d0c7e6bba135d09ba10a98
Rahul Kumar Yadav <ry573870@gmail.com>
2025-03-22 08:41:11 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import cors from 'cors';
+import path from "path"
+
 
 import productRoutes from "./routes/product.route.js";
 
@@ -10,9 +12,19 @@ dotenv.config();
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(5000, ()=>{
     console.log("Server started at https://localhost:" + PORT);   
